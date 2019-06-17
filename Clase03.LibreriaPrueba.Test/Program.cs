c8fb35c05529e04efb76796a57e5b098ca009962
santiago rouaux <sanrouaux@hotmail.com>
2019-06-17 20:31:20 -0300

    Arreglo repo

---
+++ Clase03.LibreriaPrueba.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria;
+
+namespace Proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hola, mundo!");
+            Console.ReadKey();
+
+            Console.Write("Ingrese su nombre: ");
+            String nombre;
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Su nombre es: {0}", nombre);
+            Console.ReadKey();*/
+
+            Console.WriteLine(Prueba.Saludar());
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Clase18.ProyectoEstacionamiento.TestsUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prueba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prueba")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("137dc838-b859-4c29-a028-e8fe70313a8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
